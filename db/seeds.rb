ae026e0dc7ad315db83d4409799557c3016a3d5f
taro.kirameki <fooleeqaa@gmail.com>
2020-03-16 06:10:52 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "管理者",
-             email: "marimo@kanri.com",
-             password:  "tousei0411",
-             password_confirmation: "tousei0411",
-             admin: true)
+
